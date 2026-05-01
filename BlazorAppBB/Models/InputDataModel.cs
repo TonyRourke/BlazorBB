4de5996420082575e313aabc8627f0df6e45d061
Tony <antonyrourke@hotmail.com>
2026-05-01 16:35:37 +0100

    add Date field

--- BlazorAppBB/Models/InputDataModel.cs
+++ BlazorAppBB/Models/InputDataModel.cs
@@ -7,6 +7,9 @@ namespace BlazorAppBB.Models
         [Required(ErrorMessage = "Please provide your name"), MinLength(1),MaxLength(1000, ErrorMessage = "Please limit your input to 1000 characters")]
         public string sName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Please provide the date")]
+        public DateOnly sMonth { get; set; } = DateOnly.FromDayNumber(0);
+
         [Required, Range(0.1, 9999999.99,ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
         public double dData1 { get; set; } = 0.0;
         [Required, Range(0.1, 9999999.99, ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
