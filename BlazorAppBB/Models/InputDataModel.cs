ec685385251f92644a3d1722e8aa704f6e7f4402
Tony <antonyrourke@hotmail.com>
2026-05-01 16:12:18 +0100

    input data razor with validation - tbc

---
+++ BlazorAppBB/Models/InputDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppBB.Models
+{
+    public class InputDataModel
+    {
+        [Required(ErrorMessage = "Please provide your name"), MinLength(1),MaxLength(1000, ErrorMessage = "Please limit your input to 1000 characters")]
+        public string sName { get; set; } = string.Empty;
+
+        [Required, Range(0.1, 9999999.99,ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
+        public double dData1 { get; set; } = 0.0;
+        [Required, Range(0.1, 9999999.99, ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
+
+        public double dData2 { get; set; } = 0.0;
+        [Required, Range(0.1, 9999999.99, ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
+
+        public double dData3 { get; set; } = 0.0;
+    }
+}
