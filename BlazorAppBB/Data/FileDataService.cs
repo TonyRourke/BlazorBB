04b5bf124c917d6fc056620834d26c53c454f189
Tony <antonyrourke@hotmail.com>
2026-05-13 22:00:46 +0100

    Input data table

---
+++ BlazorAppBB/Data/FileDataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using BlazorAppBB.Models;
+using System.Text.Json;
+
+namespace BlazorAppBB.Data
+{
+    public class FileDataService
+    {
+        private readonly string _filePath = "D:\\Temp\\Apes.json";
+
+        public async Task<List<InputDataModel>> GetDataAsync()
+        {
+            try
+            {
+                using var reader = new StreamReader(_filePath);
+                //var json = await reader.ReadToEndAsync();
+                var json = File.ReadAllText(_filePath);
+                return JsonSerializer.Deserialize<List<InputDataModel>>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading data: {ex.Message}");
+                return new List<InputDataModel>();
+            }
+        }
+
+        public async Task<List<InputDataModel>> SetDataAsync(List<InputDataModel> idm)
+        {
+            using var writer = new StreamWriter(_filePath);
+            var json = JsonSerializer.Serialize(idm);
+            await writer.WriteAsync(json);
+            return idm;
+        }
+        
+    }
+}
--- BlazorAppBB/Models/FinanceData.cs
+++ BlazorAppBB/Models/FinanceData.cs
@@ -2,17 +2,15 @@
 
 namespace BlazorAppBB.Models
 {
-    public class InputDataModel
+    public class FinanceData
     {
-        public int Id { get; set; }
-
-        [Required(ErrorMessage = "Please provide your name"), MinLength(1),MaxLength(1000, ErrorMessage = "Please limit your input to 1000 characters")]
+        [Required(ErrorMessage = "Please provide your name"), MinLength(1), MaxLength(1000, ErrorMessage = "Please limit your input to 1000 characters")]
         public string sName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please provide the date")]
         public DateOnly sMonth { get; set; } = DateOnly.FromDayNumber(0);
 
-        [Required, Range(0.1, 9999999.99,ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
+        [Required, Range(0.1, 9999999.99, ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
         public double dData1 { get; set; } = 0.0;
         [Required, Range(0.1, 9999999.99, ErrorMessage = "Please enter a number between 0.1 and 9999999.99")]
 
